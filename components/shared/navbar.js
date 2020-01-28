dd138ab65576183353a49968386caecc01805d9e
janakhpon <jnovaxer@gmail.com>
2020-01-28 20:54:06 +0630

    GDEV014

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Drawer from '@material-ui/core/Drawer';
@@ -12,13 +10,9 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import HelpIcon from '@material-ui/icons/Help';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
-import MuiLink from '@material-ui/core/Link';
-import ProTip from '../../src/ProTip';
 import Link from '../../src/Link';
 
 const useStyles = makeStyles(theme => ({
@@ -58,18 +52,18 @@ export default function PageNavbar() {
             onKeyDown={toggleDrawer(side, false)}
         >
             <List>
-                <ListItem button key={`Home`}>
+                <ListItem button key={`Home`} component={Link} naked href="/">
                     <ListItemIcon><HomeIcon /></ListItemIcon>
                     <ListItemText primary={`GO HOME`} />
                 </ListItem>
-                <ListItem button key={`About`}>
+                <ListItem button key={`About`} component={Link} naked href="/about">
                     <ListItemIcon><InfoIcon /></ListItemIcon>
                     <ListItemText primary={`GO ABOUT`} />
                 </ListItem>
             </List>
             <Divider />
             <List>
-                <ListItem button key={`Help`}>
+                <ListItem button key={`Help`} component={Link} naked href="/help">
                     <ListItemIcon><HelpIcon /></ListItemIcon>
                     <ListItemText primary={`GET HELP`} />
                 </ListItem>
